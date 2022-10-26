e5bd43a699176c1c7ae7ea4ac1d754dc911fc53a
PAUL AYALA <paul.ayala21@itca.edu.sv>
2022-10-26 17:32:51 -0600

    cambios perrones

--- build/web/js/Maquina.js
+++ build/web/js/Maquina.js
@@ -4,3 +4,34 @@
  */
 
 
+$('body').on('click','#editar', function(){
+    llenarFormulario($(this).closest('tr'))
+});
+
+$('body').on('click','#eliminar', function(){
+    llenarFormulario($(this).closest('tr'))
+});
+
+
+function llenarFormulario(fila){
+    var codigo = $(fila).children('td:eq(0)').html();
+    var nombre = $(fila).children('td:eq(1)').html();
+    var modelo = $(fila).children('td:eq(2)').html();
+    var marca = $(fila).children('td:eq(3)').html();
+    var tipo = $(fila).children('td:eq(4)').html();
+    var Operativo = $(fila).children('td:eq(5)').html();
+    var estado = $(fila).children('td:eq(6)').html();
+    
+    $('#txtIdM').val(codigo);
+    $('#txtNombreM').val(nombre);
+    $('#txtModelo').val(modelo);
+    $('#txtMarca').val(marca);
+    $('#txtTipo').val(tipo);
+    $('#txtOperatividad').val(Operativo);
+    $('#txtEstado').val(estado);
+    
+}
+
+function limpiarFormulario() {
+    document.getElementById("modalM").reset();
+  }
