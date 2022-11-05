447fcd0b53fae3a8723ba8e0dd595b08bf00f1bf
Marcela Calero <marcela.calero21@itca.edu.sv>
2022-11-05 14:38:42 -0600

    Actualizacion

--- src/java/com/controlador/EmpleadoServlet.java
+++ src/java/com/controlador/EmpleadoServlet.java
@@ -37,7 +37,7 @@ public class EmpleadoServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, Exception {
         response.setContentType("text/html;charset=UTF-8");
         try ( PrintWriter out = response.getWriter()) {
             
